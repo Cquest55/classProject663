663165843f2017395717d40712690c8790428cd4
blandrumjeffries <blandrumjeffries@proton.me>
2023-09-10 17:59:16 +0200

    Update navbar and header feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements AfterViewInit {
   title = 'Recipe';
+
+  @ViewChild('helloModal') helloEl?: ElementRef;
+  modal?: bootstrap.Modal;
+  ngAfterViewInit() {
+    this.modal = new bootstrap.Modal(this.helloEl?.nativeElement, {});
+  }
+
+  trigger() {
+    this.modal?.toggle();
+  }
 }
